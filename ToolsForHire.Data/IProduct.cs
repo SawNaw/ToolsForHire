f970ba1de0820b86dd762b6ef61d543c6636f26e
Saw Naw <waterchan@gmail.com>
2019-10-16 16:16:25 +0100

    Add project files.

---
+++ ToolsForHire.Data/IProduct.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolsForHire.Data
+{
+    public interface IProduct
+    {
+        IEnumerable<Product> GetAllProducts();
+    }
+
+    /// <summary>
+    /// Temporary. Replace with database.
+    /// </summary>
+    public class InMemoryProductData : IProduct
+    {
+        private readonly List<Product> Products;
+
+        public InMemoryProductData()
+        {
+            Products = new List<Product>()
+            {
+                new Product { ProductCode = "15/0115-h", ProductName = "Ground Compactor", DailyHireCost = 2.35M },
+                new Product { ProductCode = "15/1234-h", ProductName = "Hammer Drill", DailyHireCost = 3.50M },
+                new Product { ProductCode = "15/2468-h", ProductName = "Dust Extractor", DailyHireCost = 4.20M },
+                new Product { ProductCode = "15/0011-h", ProductName = "Angle Grinder", DailyHireCost = 2.88M },
+                new Product { ProductCode = "22/0447-h", ProductName = "Reciprocating Saw", DailyHireCost = 12.75M },
+                new Product { ProductCode = "07/0385-h", ProductName = "Circular Saw", DailyHireCost = 4.02M },
+                new Product { ProductCode = "55/4398-h", ProductName = "Glass Hammer", DailyHireCost = 30.00M },
+                new Product { ProductCode = "08/5478-h", ProductName = "Dust Buster", DailyHireCost = 22.00M },
+                new Product { ProductCode = "23/8720-h", ProductName = "Ground Spike", DailyHireCost = 8.00M },
+            };
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return Products.OrderBy(x => x.ProductName)
+                           .ThenBy(x => x.ProductCode)
+                           .ThenBy(x => x.DailyHireCost);
+        }
+    }
+}
---
+++ ToolsForHire.Data/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace ToolsForHire.Data
+{
+    public class Product
+    {
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public decimal DailyHireCost { get; set; }
+        
+
+        public Product(string productCode, string productName, decimal dailyHireCost)
+        {
+            ProductCode = productCode;
+            ProductName = productName;
+            DailyHireCost = dailyHireCost;
+        }
+
+        public Product()
+        {
+
+        }
+    }
+}
