ce865c589fb634a0ee1b17ac14c65dfc8e3a1603
Saw Naw <waterchan@gmail.com>
2019-10-16 16:29:22 +0100

    Give the List page model access to the list of products, which are stored in memory for now.

---
+++ ToolsForHire/Pages/List.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using ToolsForHire.Data;
+
+namespace ToolsForHire.Pages
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IProductData productData;
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public ListModel(IConfiguration config, IProductData productData)
+        {
+            this.config = config;
+            this.productData = productData;
+        }
+
+        public void OnGet()
+        {
+            // Give this List page model access to all products, so that it can list them.
+            Products = productData.GetAllProducts();
+        }
+    }
+}
