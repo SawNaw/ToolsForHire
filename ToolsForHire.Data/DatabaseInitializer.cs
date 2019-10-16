36d28664b984a5f111fe9f64c60dd668dc9702d3
Saw Naw <waterchan@gmail.com>
2019-10-16 19:43:57 +0100

    Added database seeds

---
+++ ToolsForHire.Data/DatabaseInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolsForHire.Data
+{
+    public static class DatabaseInitializer
+    {
+        public static void Initialize(ProductContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Products.Any())
+            {
+                return;  // Database contains values, so no need to seed.
+            }
+
+            var productSeeds = new Product[]
+            {
+                new Product { ProductCode = "15/0115-h", ProductName = "Ground Compactor", DailyHireCost = 2.35M },
+                new Product { ProductCode = "15/1234-h", ProductName = "Hammer Drill", DailyHireCost = 3.50M },
+                new Product { ProductCode = "15/2468-h", ProductName = "Dust Extractor", DailyHireCost = 4.20M },
+                new Product { ProductCode = "15/0011-h", ProductName = "Angle Grinder", DailyHireCost = 2.88M },
+                new Product { ProductCode = "22/0447-h", ProductName = "Reciprocating Saw", DailyHireCost = 12.75M },
+                new Product { ProductCode = "07/0385-h", ProductName = "Circular Saw", DailyHireCost = 4.02M },
+                new Product { ProductCode = "55/4398-h", ProductName = "Glass Hammer", DailyHireCost = 30.00M },
+                new Product { ProductCode = "08/5478-h", ProductName = "Dust Buster", DailyHireCost = 22.00M },
+                new Product { ProductCode = "23/8720-h", ProductName = "Ground Spike", DailyHireCost = 8.00M },
+            };
+            foreach (Product p in productSeeds)
+            {
+                context.Products.Add(p);
+            }
+            context.SaveChanges();
+
+        }
+       
+    }
+}
--- ToolsForHire/Program.cs
+++ ToolsForHire/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using ToolsForHire.Data;
 
 namespace ToolsForHire
 {
@@ -13,7 +15,30 @@ namespace ToolsForHire
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfItDoesNotExist(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfItDoesNotExist(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ProductContext>();
+                    DatabaseInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
