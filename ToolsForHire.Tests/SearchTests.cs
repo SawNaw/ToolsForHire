c3ef70fb7c04d4a76bbdc06bd0e6c924b9a5be1d
Saw Naw <waterchan@gmail.com>
2019-10-16 19:09:18 +0100

    Add interface and backend logic for dates.

---
+++ ToolsForHire.Tests/SearchTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ToolsForHire.Data;
+
+namespace ToolsForHire.Tests
+{
+    [TestClass]
+    public class SearchTests
+    {
+        [TestMethod]
+        public void IProductData_ExactSearchWorks()
+        {
+            // Tests will come later, since when the data source is replaced with a database,
+            // the tests will require a rewrite.
+        }
+    }
+}
--- ToolsForHire/Pages/List.cshtml.cs
+++ ToolsForHire/Pages/List.cshtml.cs
@@ -13,7 +13,15 @@ namespace ToolsForHire.Pages
     {
         private readonly IConfiguration config;
         private readonly IProductData productData;
+        
+        [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime HireStartDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime HireEndDate { get; set; }
 
         public IEnumerable<Product> Products { get; set; }
 
@@ -23,10 +31,80 @@ namespace ToolsForHire.Pages
             this.productData = productData;
         }
 
-        public void OnGet(string searchString)
+        /// <summary>
+        /// Calculates the total cost of hiring a tool.
+        /// ASSUMPTIONS: The duration is inclusive: that is, a hire that begins on the 24th and ends on
+        ///              the 25th will be counted as two days.
+        /// </summary>
+        /// <param name="startDate">The starting date of hire.</param>
+        /// <param name="endDate">The ending date of hire.</param>
+        /// <param name="dailyCost">The daily cost of hiring the tool.</param>
+        /// <returns></returns>
+        public decimal CalculateTotalCostOfHire(DateTime startDate, DateTime endDate, decimal dailyCost)
         {
-            this.SearchString = searchString;
+            // Get the total number of days between the start.
+            int totalDays = (endDate - startDate).Days + 1;
 
+            // Subtract the number of Sundays from the total days.
+            totalDays -= GetNumberOfSundaysBetween(startDate, endDate);
+
+            // Subtract the number of Saturdays from the total days.
+            totalDays -= GetNumberOfSaturdaysBetween(startDate, endDate);
+
+            // ...but DO include Saturdays if the start date or end date falls on a Saturday
+            if (startDate.DayOfWeek == DayOfWeek.Saturday)
+            {
+                ++totalDays;
+            }
+            if (endDate.DayOfWeek == DayOfWeek.Saturday)
+            {
+                ++totalDays;
+            }
+
+            return totalDays * dailyCost;
+
+        }
+
+        /// <summary>
+        /// Gets the number of Sundays between two dates.
+        /// </summary>
+        /// <param name="startDate">The starting date.</param>
+        /// <param name="endDate">The ending date.</param>
+        /// <returns></returns>
+        public int GetNumberOfSundaysBetween(DateTime startDate, DateTime endDate)
+        {
+            return GetDaysBetween(startDate, endDate)
+                   .Where(d => d.DayOfWeek == DayOfWeek.Sunday).Count();
+        }
+
+        /// <summary>
+        /// Gets the number of Saturdays between two dates.
+        /// </summary>
+        /// <param name="startDate">The starting date.</param>
+        /// <param name="endDate">The ending date.</param>
+        /// <returns></returns>
+        public int GetNumberOfSaturdaysBetween(DateTime startDate, DateTime endDate)
+        {
+            return GetDaysBetween(startDate, endDate)
+                   .Where(d => d.DayOfWeek == DayOfWeek.Saturday).Count();
+        }
+
+        /// <summary>
+        /// Gets the days between two dates.
+        /// </summary>
+        /// <param name="start">The starting date.</param>
+        /// <param name="end">The ending date.</param>
+        /// <returns></returns>
+        IEnumerable<DateTime> GetDaysBetween(DateTime start, DateTime end)
+        {
+            for (DateTime i = start; i < end; i = i.AddDays(1))
+            {
+                yield return i;
+            }
+        }
+
+        public void OnGet()
+        {
             if (String.IsNullOrWhiteSpace(SearchString))
             {
                 Products = productData.GetAllProducts();
